83992dd91a75cce28b6fba042fada4ba6f307f46
Eduard Schmidt <eduard.schmidt@posteo.net>
2022-05-02 23:57:00 +0200

    Added the sewating animation

--- Software/buddy.py
+++ Software/buddy.py
@@ -14,7 +14,12 @@ class State(Enum):
     ASLEEP = 1
     WAKE_UP = 2
     AWAKE = 3
-    EXIT =9999
+    NORMAL = 5
+    COLD = 10
+    HOT = 11
+    THURSTY = 20
+    DROWNING = 21
+    EXIT = 9999
 
 class Emoji(Enum):
     HEART_SMALL = 1
@@ -25,9 +30,9 @@ class Emoji(Enum):
     SMILE_BLINK = 6
     KISS = 7
     YAWNING_CLOSED_EYES = 8
-    YAWNING_OPEND_EYES = 9
+    YAWNING_OPENED_EYES = 9
     FLAT_CLOSED_EYES = 10
-    FLAT_OPEND_EYES = 11
+    FLAT_OPENED_EYES = 11
     SMILE_BLINK_TONGUE_OUT = 12
 
 class Animation(Enum):
@@ -40,6 +45,7 @@ class Animation(Enum):
     SLEEP = 7
     LOOK_LEFT_RIGHT_TONGUE_OUT = 8
     ROLE_FACE = 9
+    SWEATING = 10
 
 
 class Buddy():
@@ -52,6 +58,7 @@ class Buddy():
         
         #Init the State machine
         self.state = State.INIT
+        self.plantState  = State.HOT #Init the plant state to normal
 
 
     def run(self):
@@ -64,17 +71,12 @@ class Buddy():
 
         print("Buddy is up and awake")
 
-    def updateState(self, state:State):
+    def updatePlantState(self, state:State):
         """
         Send the Plant State to the plant buddy
         ----
         state is the plant state as PlantState Enum  
         """
-
-        #Check if the state has changed
-        if (self.plantState != state):
-            self.plantStateSwitch = True
-
         #safe the state to the local variable
         self.plantState = state
 
@@ -119,21 +121,50 @@ class Buddy():
                 #Perform the wake up animation
                 self.plantStateSwitch = False
 
-                #Clear
-                while not self.plantStateSwitch:
+                animationSelector = random.randrange(100)
 
-                    animationSelector = random.randrange(100)
+                if (self.plantState != State.NORMAL):
+                    self.state = self.plantState
 
-                    if(animationSelector < 75):
-                        self.__animation(Animation.RANDOM_BLINK)
-                    elif(animationSelector < 85):
-                        self.__animation(Animation.LOOK_LEFT_RIGHT_TONGUE_OUT)
-                    elif(animationSelector < 90):
-                        self.__animation(Animation.ROLE_FACE)
-                    elif(animationSelector < 95):
-                        self.__animation(Animation.LOOK_LEFT_RIGHT_BLINK)
-                    elif(animationSelector < 100):
-                        self.__animation(Animation.LOOK_LEFT_RIGHT_KISS)
+                elif(animationSelector < 75):
+                    self.__animation(Animation.RANDOM_BLINK)
+                elif(animationSelector < 85):
+                    self.__animation(Animation.LOOK_LEFT_RIGHT_TONGUE_OUT)
+                elif(animationSelector < 90):
+                    self.__animation(Animation.ROLE_FACE)
+                elif(animationSelector < 95):
+                    self.__animation(Animation.LOOK_LEFT_RIGHT_BLINK)
+                elif(animationSelector < 100):
+                    self.__animation(Animation.LOOK_LEFT_RIGHT_KISS)
+
+            elif(self.state == State.COLD):
+                #To cold for the plant
+                print("State == Cold")
+                time.sleep(1)
+                False #Performe the animation for to cold
+                
+                self.state = State.AWAKE
+            
+            elif(self.state == State.HOT):
+                #To hot for the plant
+                print("State == Hot")
+
+                self.__animation(Animation.SWEATING)
+                self.state = State.AWAKE
+
+            elif(self.state == State.THURSTY):
+                #To less water for the plant
+                print("State == Thursty")
+                time.sleep(1)
+                False 
+                self.state = State.AWAKE
+
+            elif(self.state == State.DROWNING):
+                #To much water
+                print("State == Drowning")
+                time.sleep(1)
+                False
+                self.state = State.AWAKE
 
 
             self.plantStateSwitch = False
@@ -171,13 +202,13 @@ class Buddy():
         elif(Emoji.YAWNING_CLOSED_EYES == emoji):
             icon = b"\x00\x00\x24\x00\x2A\x00\x0A\x00\x0A\x00\x2A\x00\x24\x00\x00\x00"
 
-        elif(Emoji.YAWNING_OPEND_EYES == emoji):
+        elif(Emoji.YAWNING_OPENED_EYES == emoji):
             icon = b"\x00\x00\x64\x00\x6A\x00\x0A\x00\x0A\x00\x6A\x00\x64\x00\x00\x00"
 
         elif(Emoji.FLAT_CLOSED_EYES == emoji):
             icon = b"\x00\x00\x24\x00\x24\x00\x04\x00\x04\x00\x24\x00\x24\x00\x00\x00"
 
-        elif(Emoji.FLAT_OPEND_EYES == emoji):
+        elif(Emoji.FLAT_OPENED_EYES == emoji):
             icon = b"\x00\x00\x64\x00\x64\x00\x04\x00\x04\x00\x64\x00\x64\x00\x00\x00"
 
         elif(Emoji.SMILE_BLINK_TONGUE_OUT == emoji):
@@ -214,15 +245,15 @@ class Buddy():
             time.sleep(0.7)
             self.__drawEmoji(Emoji.FLAT_CLOSED_EYES)
             time.sleep(0.2)
-            self.__drawEmoji(Emoji.FLAT_OPEND_EYES)
+            self.__drawEmoji(Emoji.FLAT_OPENED_EYES)
             time.sleep(0.2)
             self.__drawEmoji(Emoji.FLAT_CLOSED_EYES)
             time.sleep(0.2)
-            self.__drawEmoji(Emoji.FLAT_OPEND_EYES)
+            self.__drawEmoji(Emoji.FLAT_OPENED_EYES)
             time.sleep(0.7)
-            self.__drawEmoji(Emoji.YAWNING_OPEND_EYES)
+            self.__drawEmoji(Emoji.YAWNING_OPENED_EYES)
             time.sleep(0.8)
-            self.__drawEmoji(Emoji.FLAT_OPEND_EYES)
+            self.__drawEmoji(Emoji.FLAT_OPENED_EYES)
             time.sleep(0.7)
             self.__drawEmoji(Emoji.SMILE_LOOK_CENTER)
 
@@ -344,5 +375,62 @@ class Buddy():
             self.__drawIcon(_icon)
             time.sleep(_sleepTime)
             _icon = b"\x00\x00\x00\x00\x64\x00\x62\x00\x02\x00\x02\x00\x62\x00\x64\x00"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+
+        elif (Animation.SWEATING == animation):
+            # Sweating face
+
+            _sleepTime = 0.2
+
+            _icon = b"\x00\x00\x64\x00\xEA\x80\x08\x03\x08\x03\x6A\x00\x64\x00\x00\x00"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x00\x00\xE4\x80\x6A\x00\x08\x03\x08\x03\x6A\x00\x64\x00\x00\x00"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x80\x80\x64\x00\x6A\x00\x08\x03\x08\x03\x6A\x00\x64\x00\x00\x00"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x40\x40\x64\x00\x6A\x00\x08\x03\x08\x03\x2A\x00\x24\x00\x00\x00"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x20\x20\x24\x00\x2A\x00\x08\x03\x08\x03\x2A\x00\x24\x00\x00\x00"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x10\x10\x24\x00\x2A\x00\x08\x03\x88\x83\x2A\x00\x24\x00\x00\x00"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x08\x08\x24\x00\x2A\x00\x08\x03\x08\x03\xEA\x80\x64\x00\x00\x00"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x04\x04\x64\x00\x6A\x00\x08\x03\x08\x03\x6A\x00\xE4\x80\x00\x00"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x02\x02\x64\x00\x6A\x00\x08\x03\x08\x03\x6A\x00\x64\x00\x80\x80"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x01\x01\x64\x00\x6A\x00\x08\x03\x08\x03\x6A\x00\x64\x00\x40\x40"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x00\x00\x64\x00\x6A\x00\x08\x03\x08\x03\x6A\x00\x64\x00\x20\x20"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x00\x00\x64\x00\x6A\x00\x08\x03\x08\x03\x6A\x00\x64\x00\x10\x10"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x00\x00\x64\x00\x6A\x00\x08\x03\x08\x03\x6A\x00\x64\x00\x08\x08"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x00\x00\x64\x00\x6A\x00\x08\x03\x08\x03\x2A\x00\x24\x00\x04\x04"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x00\x00\x24\x00\x2A\x00\x08\x03\x08\x03\x2A\x00\x24\x00\x02\x02"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x00\x00\x24\x00\x2A\x00\x08\x03\x08\x03\x2A\x00\x24\x00\x01\x01"
+            self.__drawIcon(_icon)
+            time.sleep(_sleepTime)
+            _icon = b"\x00\x00\x64\x00\x6A\x00\x08\x03\x08\x03\x2A\x00\x24\x00\x00\x00"
             self.__drawIcon(_icon)
             time.sleep(_sleepTime)
